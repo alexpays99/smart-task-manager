b3d1fc100d126299371631b0afff1d2616d2e370
alex.mosisa <alex.mosisa@chisw.com>
2025-02-09 16:39:35 +0200

    improvement: added  swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   // Swagger Configuration
+   const config = new DocumentBuilder()
+   .setTitle('Smart Task Meneger API')
+   .setDescription('API documentation for Smart Task Meneger')
+   .setVersion('1.0')
+   .addBearerAuth() // if use authorization (optional)
+   .build();
+ const document = SwaggerModule.createDocument(app, config);
+ SwaggerModule.setup('api-docs', app, document);
+  
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
